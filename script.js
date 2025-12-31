d35f06d901b33d494de891422047f073a7002d37
BSTD-DOMINO <ddrycko@gmail.com>
2025-12-31 15:31:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,24 +7,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const btnWork = document.getElementById('btn-work');
     const workResult = document.getElementById('work-result');
 
-  
+    
     btnCheck.addEventListener('click', () => {
-       
+        
         bonusResult.style.display = 'block';
-       
     });
 
     
     btnWork.addEventListener('click', () => {
         
         btnWork.innerHTML = '✔️ ПРИЙНЯТО';
-       
+        
+        
         btnWork.classList.remove('btn-red');
         btnWork.classList.add('btn-green');
         
-        
         btnWork.disabled = true;
-
         
         workResult.style.display = 'block';
     });
